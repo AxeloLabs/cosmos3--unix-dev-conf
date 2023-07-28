1bbc7aa49c4a4bcdedaad00073db5dd8bceff7cd
Axel <133879412+nzaero@users.noreply.github.com>
2023-07-28 05:49:05 +0200

    ⬛ WIP [[Fri Jul 28 05:49:05 CEST 2023]]

--- 1-alias/CLI/3-chrome.sh
+++ 1-alias/CLI/3-chrome.sh
@@ -5,3 +5,8 @@
 #              '
 alias chrome--green='/Applications/Google\ Chrome--green.app/Contents/MacOS/Google\ Chrome \
                      --user-data-dir=~/0gh/0-chromeData/datadir-green'
+
+ln -s "$PWD/build" /Applications/Google\ Chrome--green.app/Contents/MacOS/Google\ Chrome \
+    --user-data-dir=/tmp/foo --ignore-certificate-errors \
+    --unsafely-treat-insecure-origin-as-secure=https://localhost \
+    https://localhost/demos/svelte-wp-display
